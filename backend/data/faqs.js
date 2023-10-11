ee23c74abe018a3f654b7e6a88184daa6297a978
thisislaait <ogadi.ugo@gmail.com>
2023-10-11 22:22:29 +0200

    API data created and dotenv installed

---
+++ backend/data/faqs.js
@@ -0,0 +1,65 @@
+const faqs = [
+    {
+        id: "1",
+        title: "Market Expansion",
+        content: 
+            "Expand your restaurant's reach to new geographical areas and customer segments for increased revenue and growth",
+        category: "navbar_services",
+    },
+    {
+        id: "2",
+        title: "Data-Driven Insights",
+        content: "HArness data analytics and make informed decisions. Optimize operations and enhance profitability",
+        category: "navbar_services",
+    },
+    {
+        id: "3",
+        title: "Sales Optimization",
+        content: "Maximize revenue with efficient sales tracking, promotions and customer engagement strategies",
+        category: "navbar_services",
+    },
+    {
+        id: "4",
+        title: "Streamlined Pre-orders",
+        content: "Simplify the ordering process for customers and boost operational efficiency for later service.",
+        category: "navbar",
+    },
+    {
+        id: "5",
+        title: "Physical Restaurants",
+        content: "Run a traditional brick-and-mortar restaurant, offering dine-in services, takeout, and potentially delivery through the platform, enhancing your restaurant's visibility and customer reach",
+        category: "navbar_clients",
+    },
+    {
+        id: "6",
+        title: "Virtual Restaurants",
+        content: "Establish and operate a thriving online-only food delivery business, leveraging the platform to attract customers, increase sales, and maximize your online presence",
+        category: "navbar_clients",
+    },
+    {
+        id: "7",
+        title: "Pop-up Restaurants",
+        content: "Establish and operate a thriving online-only food delivery business, leveraging the platform to attract customers, increase sales, and maximize your online presence",
+        category: "navbar_clients",
+    },
+    {
+        id: "8",
+        title: "Fine Dining Restaurants",
+        content: "Establish and operate a thriving online-only food delivery business, leveraging the platform to attract customers, increase sales, and maximize your online presence.",
+        category: "navbar_clients",
+    },
+    {
+        id: "9",
+        title: "Buffet Restaurants",
+        content: "Present your buffet-style restaurant to a wider audience, allowing customers to view your menu offerings, pricing and availability through the platform",
+        category: "navbar_clients",
+    },
+    {
+        id: "10",
+        title: "Market Expansion",
+        content: "Expand your restaurant's reach to new geographical areas and customer segments for increased revenue and growth",
+        category: "navbar",
+    },
+];
+
+module.exports = faqs;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const faqs = require('./data/faqs');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config()
 
-app.listen(5000, console.log("server started"));
+app.get("/", (req,res) => {
+    res.send("API is running...")
+});
+
+app.get("/api/faqs", (req,res) => {
+    res.json(faqs);
+});
+
+app.get("/api/faqs/:id", (req,res) => {
+    const faq=faqs.find((n)=>n.id === req.params.id);
+
+    res.send(faq);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("server started"));
